dd7f5664b6a0072312b39b05fddfd889a809831b
AlexStorm <info@alexstorm.ru>
2023-12-29 04:01:58 +0530

    build conf

--- build/build.ts
+++ build/build.ts
@@ -2,18 +2,17 @@ console.log('Building...');
 
 import { SolidPlugin } from "./plugin";
 
-const bun =Bun.FileSystemRouter
-
-const start=Bun.nanoseconds()
+const start = Bun.nanoseconds()
 
 await Bun.build({
-    entrypoints: ['./src/index.ts'],
-    outdir: './dist',
-    external: ['solid-js', 'solid-js/web'],
-    plugins: [SolidPlugin()],
-  }).catch((err) => {
-    console.error(err);}).then(() => {
-      
-const end=Bun.nanoseconds()
-const time = (end - start)/1000000
-        console.log('Build complete! ',time);});
+  entrypoints: ['./src/index.ts'],
+  outdir: './dist',
+  external: ['solid-js', 'solid-js/web'],
+  plugins: [SolidPlugin()],
+}).catch((err) => {
+  console.error(err);
+}).then(() => {
+  const end = Bun.nanoseconds();
+  const time = (end - start) / 1000000;
+  console.log('Build complete! ', time);
+});
