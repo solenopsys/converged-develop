e520d3683c534bacc65d15a3b7ee01563ce4caa6
Alex Storm <info@alexstorm.ru>
2024-01-06 19:40:34 +0530

    layout refactor

--- packages/solenopsys/lt-website/src/layout/layout.tsx
+++ packages/solenopsys/lt-website/src/layout/layout.tsx
@@ -2,79 +2,41 @@ import { Dynamic } from "solid-js/web";
 import { createSignal, Component, createResource } from "solid-js";
 import styles from "./layout.module.css"
 import "./layout.css"
-import { UiTreeMenu, UiTabs,UiTopPane } from "@solenopsys/ui-navigate";
-import { MdDynamic } from "./mddynamic"
-import { Router, Route, A } from "@solidjs/router";
+
 import { UiButton } from '@solenopsys/ui-controls';
 
-
-
-const Empty: Component = () => {
-    return (<div>Empty</div>)
-}
-
-const EMPTY: string = "EMPTY"
-const MENU: string = "MENU"
-const MD: string = "MD"
-const TABS: string = "TABS"
-
-const components: { [name: string]: Component } = {
-    EMPTY: Empty
-}
-
-const fetchMenuData = async () =>
-    (await fetch(`/dag?key=menu&cid=bafyreicpz3bnf3xqabciyypjssfue54csygb3fn4soz3wbztppvzdfahsy`)).json();
-
-
-const [menuData] = createResource(fetchMenuData);
-
-
-const tabs = [{ id: "tab1", title: "Title1" }, { id: "tab2", title: "Title2" }]
-
- 
-
-components[MENU] = () => { return <> {menuData() && <UiTreeMenu data={menuData()} baseUrl="/article"/>}</> }
-
-components[TABS] = () => { return <> {<UiTopPane logo={"/images/logo.svg"} tabsState={tabs} />} </> }
-
-components[MD] = () => {
-    return (
-        <Router>
-            <Route path="/article/:id" component={MdDynamic} />
-        </Router>
-    )
+interface MdItemComponentProps {
+    top: Component,
+    central: Component,
+    left: Component,
+    components: { [name: string]: Component }
 }
 
 
-const [top, setTop] = createSignal(EMPTY);
-const [central, setCentral] = createSignal(EMPTY);
-const [left, setLeft] = createSignal(EMPTY);
-
-
-setLeft(MENU)
-setCentral(MD)
-setTop(TABS)
-
-
-export const SiteLayout: Component = () => {
+export const SiteLayout: Component<MdItemComponentProps> = (props) => {
     const [mobileMenu, mobileMenuTop] = createSignal(false);
+
+    const [top, setTop] = createSignal(props.top);
+    const [central, setCentral] = createSignal(props.central);
+    const [left, setLeft] = createSignal(props.left);
+
 
     return (
         <div class={styles.body_wrapper}>
             <div class={styles.full_height}>
                 <div class={styles.top_pane_wrapper} >
-                    <Dynamic component={components[top()]} />
+                    <Dynamic component={props.components[top()]} />
                 </div>
                 <div class={styles.left_block}>
                     <div class={mobileMenu() ? styles.main_menu_mobile : styles.main_menu} >
                         <div class={styles.main_menu_wrapper}  >
-                            <Dynamic component={components[left()]} />
+                            <Dynamic component={props.components[left()]} />
                         </div>
                     </div>
                 </div>
                 <div class={styles.main_content}>
-                <UiButton title="bla" ></UiButton>
-                    <Dynamic component={components[central()]} />
+                    <UiButton title="bla" ></UiButton>
+                    <Dynamic component={props.components[central()]} />
                 </div>
             </div>
 
