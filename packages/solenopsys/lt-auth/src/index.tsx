84d00fa38efbca58a4e27371c06d1a24c2e95c09
AlexStorm <info@alexstorm.ru>
2024-04-27 09:43:48 +0530

    google auth

--- packages/solenopsys/lt-auth/src/index.tsx
+++ packages/solenopsys/lt-auth/src/index.tsx
@@ -1,76 +1,70 @@
-import { lazy, render } from "@solenopsys/converged-renderer";
-
-import { Site } from "./layout/site";
+import { If, render } from "@solenopsys/converged-renderer";
+import $ from "@solenopsys/converged-reactive";
 import { Router } from "@solenopsys/converged-router";
 import { UiEvents } from "@solenopsys/converged-renderer";
+import { googleOneTap } from "./gin";
+import {jwtDecode} from 'jwt-decode';
 
-function setPageTitle(title:string){
-	document.title=title
+
+
+
+function setPageTitle(title: string) {
+	document.title = title;
 }
 
-function setFavicon(href:string){
-	const link=document.querySelector("link[rel*='shortcut icon']") as HTMLAnchorElement | null
-	if(link !== null){
-		link.href=href
+function setFavicon(href: string) {
+	const link = document.querySelector(
+		"link[rel*='shortcut icon']",
+	) as HTMLAnchorElement | null;
+	if (link !== null) {
+		link.href = href;
 	}
 }
 
 
-function initGoogleApi(){
-	const script = document.createElement("script");
-	script.src = "https://apis.google.com/js/platform.js";
-	script.async = true;
-	script.defer = true;
-	document.body.appendChild(script);
-}
+const options = {
+	client_id: '839277684918-b4ji9lbh0a2t3qi48t9b9mb0r027l20a.apps.googleusercontent.com', // required
+	auto_select: this, // optional
+	cancel_on_tap_outside: false, // optional
+	context: 'signin', // optional
+};
 
 
 
 
-function onSignIn(googleUser) {
-	var profile = googleUser.getBasicProfile();
-	console.log('ID: ' + profile.getId());
-	console.log('Name: ' + profile.getName());
-	console.log('Image URL: ' + profile.getImageUrl());
-	console.log('Email: ' + profile.getEmail());
-  }
 
-  function renderButton() {
-	gapi.signin2.render('my-signin2', {
-	  'scope': 'profile email',
-	  'width': 240,
-	  'height': 50,
-	  'longtitle': true,
-	  'theme': 'dark',
-	  'onsuccess': onSignIn
-	});
-  }
-  
-  gapi.load('auth2', function() {
-	gapi.auth2.init();
-	renderButton();
-  });
 
 export const createLayout = (
 	tagId: string,
 	loadModule: (name: string) => {},
-	conf: any
+	conf: any,
 ) => {
-	UiEvents({type:"LayoutInit",tag:tagId})
-	setPageTitle(conf.page.title)
-	setFavicon(conf.page.favicon)
-	initGoogleApi()
-	renderButton() 
 	
+
+	googleOneTap(options, (response:{credential:string}) => {
+
+		console.log("GOOGLE AUTH", response);
+		// Send response to server
+
+ 
+		const jwt = localStorage.getItem('jwt');
+
+// Use jwtDecode to decode the JWT
+const decoded = jwtDecode(response.credential);
+
+		console.log( decoded);
+
+		
+	});
 	// @ts-ignore
 	render(() => {
-
-		console.log("ROUTER INIT")
+	
+		console.log("ROUTER INIT");
 		return (
 			<Router>
 				ok
-				<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-
+		
+				
 			</Router>
 		);
 	}, document.getElementById(tagId));
