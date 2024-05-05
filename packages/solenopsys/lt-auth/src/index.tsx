c0c24146f504b7a0059adf587ab70c6b42d40c18
AlexStorm <info@alexstorm.ru>
2024-05-05 20:36:06 +0530

    small refactor

--- packages/solenopsys/lt-auth/src/index.tsx
+++ packages/solenopsys/lt-auth/src/index.tsx
@@ -2,10 +2,6 @@ import { If, render } from "@solenopsys/converged-renderer";
 import $ from "@solenopsys/converged-reactive";
 import { Router } from "@solenopsys/converged-router";
 import { UiEvents } from "@solenopsys/converged-renderer";
-import { googleOneTap } from "./gin";
-import {jwtDecode} from 'jwt-decode';
-
-
 
 
 function setPageTitle(title: string) {
@@ -21,51 +17,14 @@ function setFavicon(href: string) {
 	}
 }
 
-
-const options = {
-	client_id: '839277684918-b4ji9lbh0a2t3qi48t9b9mb0r027l20a.apps.googleusercontent.com', // required
-	auto_select: this, // optional
-	cancel_on_tap_outside: false, // optional
-	context: 'signin', // optional
-};
-
-
-
-
-
-
 export const createLayout = (
 	tagId: string,
 	loadModule: (name: string) => {},
 	conf: any,
 ) => {
 	
-
-	googleOneTap(options, (response:{credential:string}) => {
-
-		console.log("GOOGLE AUTH", response);
-		// Send response to server
-
- 
-		const jwt = localStorage.getItem('jwt');
-
-// Use jwtDecode to decode the JWT
-const decoded = jwtDecode(response.credential);
-
-		console.log( decoded);
-
-		
-	});
 	// @ts-ignore
 	render(() => {
-	
-		console.log("ROUTER INIT");
-		return (
-			<Router>
-				ok
-		
-				
-			</Router>
-		);
+		return <Router>ok</Router>;
 	}, document.getElementById(tagId));
 };
